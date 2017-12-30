144e23654ed8185a4a399e5ae91850d4d003aa94
crystalDf <chendong333@gmail.com>
2017-12-30 18:36:58 +0800

    Chapter 28 PhotoGallery

---
+++ app/src/main/java/com/star/photogallery/GalleryItem.java
@@ -0,0 +1,30 @@
+package com.star.photogallery;
+
+
+public class GalleryItem {
+
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+}
